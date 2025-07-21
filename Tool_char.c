615c9a10463bf983e2681a0795271c0902387deb
root <lapique.philippe@gmail.com>
2025-07-21 22:04:15 +0000

    Eddit Tool_char.c function void tool_char(va_list note) || Eddit comment

--- Tool_char.c
+++ Tool_char.c
@@ -1,8 +1,20 @@
 #include "main.h"
 
 /**
+ *tool_char - Prints a single character from a variadic
+ *	      argument list.
  *
+ *@note: The va_list containing the arguments passed to
+ *	 _printf.
  *
+ *This function extracts the next argument as an int (since
+ *chars are promoted to int in variadic functions), casts
+ *it to a char, and writes it to stdout.
  *
  */
 
+void tool_char(va_list note)
+{
+char c = (char) va_arg(note, int);
+write(1, c, 1);
+}
