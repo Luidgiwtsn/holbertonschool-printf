a94a9cf5627b9c3b9bdd2007bbac9ccd52161d47
root <lapique.philippe@gmail.com>
2025-07-22 10:10:27 +0000

    Eddit _printf struct_array atlas

--- _printf.c
+++ _printf.c
@@ -9,13 +9,17 @@
 
 int _printf(const char *format, ...)
 {
-
 va_list note;
 va_start (note, format);
 
-tool_string(note);
-tool_char(note);
+atlas formats[] ={
+{'c', tool_char},
+{'s', tool_string},
+{'d', tool_decimal},
+{'i', tool_int},
+};
 
 va_end(note);
+
 return (0);
 }
