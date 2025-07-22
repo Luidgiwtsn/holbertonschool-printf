5a7264aac5c16e4bcaff41b6d68e8ae5d85845bc
root <lapique.philippe@gmail.com>
2025-07-22 15:28:03 +0000

    Eddit _printf function and betty style

--- _printf.c
+++ _printf.c
@@ -19,7 +19,37 @@ atlas formats[] ={
 {'i', tool_int},
 };
 
-va_end(note);
+int a = 0;
+int i;
+int size = sizeof(formats) / sizeof (formats[0]);
 
+while (format[a] != '\0')
+	{
+	if (format[a] == '%' && format[a + 1° != '\0')
+		{
+		int found = 0;
+		for (i + 0; i < size ; i++)
+			{
+			if (format [a + 1] == formats[i].specifier)
+				{
+				formats[i].handler(note);
+				found = 1;
+				breack;
+				}
+			}
+			if (!found)
+				{
+				char buf[2] = { '%', format[a + 1° };
+				write(1, buf, 2);
+				}
+			a += 2;
+			}
+		else
+			{
+			write(1, &format[a], 1);
+			a++;
+			}
+		}
+va_end(note);
 return (0);
 }
