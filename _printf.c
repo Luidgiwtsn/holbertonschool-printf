724c37b3d2ef31785c71484378012338e0d3421f
root <lapique.philippe@gmail.com>
2025-07-23 12:07:12 +0000

    Eddit _printf.c function return and boucle if for single %%

--- _printf.c
+++ _printf.c
@@ -10,46 +10,61 @@
 int _printf(const char *format, ...)
 {
 va_list note;
-va_start (note, format);
 
-atlas formats[] ={
+atlas formats[] = {
 {'c', tool_char},
 {'s', tool_string},
-{'d', tool_decimal},
+{'d', tool_int},
 {'i', tool_int},
 };
 
 int a = 0;
 int i;
 int size = sizeof(formats) / sizeof (formats[0]);
+char buf[2];
+int found = 0;
+char c = '%';
+int count = 0;
+
+va_start(note, format);
 
 while (format[a] != '\0')
 	{
-	if (format[a] == '%' && format[a + 1° != '\0')
+	if (format[a] == '%' && format[a + 1] != '\0')
 		{
-		int found = 0;
-		for (i + 0; i < size ; i++)
+		if (format[a + 1] == '%')
+			{
+			write(1, &c, 1);
+			count++;
+			a += 2;
+			continue;
+			}
+		found = 0;
+		for (i = 0; i < size; i++)
 			{
 			if (format [a + 1] == formats[i].specifier)
 				{
-				formats[i].handler(note);
+				count += formats[i].handler(note);
 				found = 1;
-				breack;
+				break;
 				}
 			}
 			if (!found)
 				{
-				char buf[2] = { '%', format[a + 1° };
+				buf[0] = '%';
+				buf[1] = format[a + 1];
 				write(1, buf, 2);
+				count += 2;
 				}
 			a += 2;
-			}
+		}
 		else
 			{
 			write(1, &format[a], 1);
+			count++;
 			a++;
 			}
-		}
+	}
 va_end(note);
-return (0);
+return count;
 }
